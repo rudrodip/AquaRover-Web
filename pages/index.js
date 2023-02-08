b3b61f6c50db8c47bacdb48f2111619403b18314
rudrodip <official.rudrodipsarker@gmail.com>
2023-02-08 15:53:04 +0600

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Water-Web</title>
+        <meta name="description" content="open source water monitoring system" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </>
+  )
+}
